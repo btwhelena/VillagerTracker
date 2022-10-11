e9255451a3ee4ddfa68b8d4e8f43f03a479b1631
Helena <helenaoliveira@alu.ufc.br>
2022-10-11 08:50:13 -0300

    Initial Commit

---
+++ VillagerTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VillagerTracker
+//
+//  Created by Helena Oliveira on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ VillagerTrackerUITests/VillagerTrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  VillagerTrackerUITestsLaunchTests.swift
+//  VillagerTrackerUITests
+//
+//  Created by Helena Oliveira on 11/10/22.
+//
+
+import XCTest
+
+final class VillagerTrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
